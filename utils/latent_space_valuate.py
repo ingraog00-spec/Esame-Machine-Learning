d7167a78dbcba6c3d7b1e87d1e72719febecff14
GiovanniIacuzzo <iacuzzogiovanni02@gmail.com>
2025-05-22 12:01:09 +0200

    Aggiornata la funzione di valutazione dello spazio latente per supportare il logging con comet_ml e migliorata la gestione del report di classificazione.

--- utils/latent_space_valuate.py
+++ utils/latent_space_valuate.py
@@ -2,29 +2,38 @@ from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
+import io
 from utils.feature_extraction import extract_embeddings_latent_space
+import comet_ml
 
-def evaluate_latent_space(model, dataloaders, device, class_names=None):
+def evaluate_latent_space(model, dataloaders, device, class_names=None, experiment=None):
     """
-    Valuta lo spazio latente del VAE usando un classificatore supervisionato.
+    Valuta lo spazio latente del VAE usando un classificatore SVM.
+    
     Args:
         model: il VAE già allenato
         dataloaders: dict con {"train": dl_train, "val": dl_val, "test": dl_test}
         device: 'cuda' 'mps' o 'cpu'
+        class_names: lista dei nomi delle classi
+        experiment: oggetto comet_ml.Experiment
     """
     print("Estrazione embeddings...")
     z_train, y_train = extract_embeddings_latent_space(model, dataloaders["train"], device)
     z_test, y_test = extract_embeddings_latent_space(model, dataloaders["test"], device)
 
     print("Addestramento classificatore (SVM)...")
-    clf = SVC(kernel='rbf', class_weight='balanced')  # kernel usato: gaussiano, oppure usare kernel='linear', 'poly', etc.
+    clf = SVC(kernel='rbf', class_weight='balanced')
     clf.fit(z_train, y_train)
 
     print("Valutazione su test set...")
     y_pred = clf.predict(z_test)
-    
+
     print("\nClassification Report:")
-    print(classification_report(y_test, y_pred, target_names=class_names))
+    report = classification_report(y_test, y_pred, target_names=class_names, output_dict=False)
+    print(report)
+
+    if experiment:
+        experiment.log_text(report, metadata={"type": "classification_report"})
 
     # Confusion matrix
     cm = confusion_matrix(y_test, y_pred)
@@ -34,4 +43,10 @@ def evaluate_latent_space(model, dataloaders, device, class_names=None):
     plt.xlabel("Predicted")
     plt.ylabel("True")
     plt.tight_layout()
-    plt.show()
+
+    if experiment:
+        buf = io.BytesIO()
+        plt.savefig("./images/confusion_matrix_latent_space_evaluate.png", buf)
+        buf.seek(0)
+        experiment.log_image(buf, name="confusion_matrix_latent_space_evaluate", image_format="png", step=None)
+        buf.close()
